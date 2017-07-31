5b37be69afc2b51cfe90d05fc4afcb10ed193f0b
leahg725 <lgazes725@gmail.com>
2017-07-31 14:31:38 -0400

    changed resume list

---
+++ js/scrollto.js
@@ -0,0 +1,16 @@
+//(function($) {
+    $('[data-smoothscroll]').click(function() {
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+          var $target = $(this.hash);
+          $target = $target.length && $target || $('[name=' + this.hash.slice(1) +']');
+          if ($target.length) {
+            var height = $('.Navigation').outerHeight(true);
+            var siteHeaderOffset = $('.Navigation').outerHeight(true) * 2;
+            var targetOffset = $target.offset().top;
+
+            $('html,body').animate({scrollTop: targetOffset}, 500);
+                return false;
+            }
+        }
+    });
+//}(jQuery));
