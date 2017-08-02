dcdb356805819be523ec38ea4eb0be0335392580
leahg725 <lgazes725@gmail.com>
2017-08-02 13:38:46 -0400

    fixed function

--- js/scrollto.js
+++ js/scrollto.js
@@ -1,4 +1,4 @@
-(function($) {
+$(document).ready(function() {
     $('[data-smoothscroll]').click(function() {
         if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
           var $target = $(this.hash);
@@ -13,4 +13,4 @@
             }
         }
     });
-}(jQuery));
+});
