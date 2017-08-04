f72bdba412ea91948c4fba75c278163ded7feee6
leahg725 <lgazes725@gmail.com>
2017-08-03 22:01:07 -0400

    added navigation styling

--- js/navopen.js
+++ js/navopen.js
@@ -1,12 +1,16 @@
 $(document).ready(function() {
-    $('.Navigation-menu_toggle').on('touchstart click', function(e) {
+    $('.Navigation-menu_toggle').on(' click', function(e) {
         if($('.Navigation-link_list_wrapper').hasClass('closed')) {
             $('.Navigation-link_list_wrapper').removeClass('closed');
             $('.Navigation-link_list_wrapper').addClass('open');
+            $('.Navigation-overlay').addClass('open');
+            $('.Navigation-overlay').removeClass('closed');
         }
         else {
             $('.Navigation-link_list_wrapper').addClass('closed');
             $('.Navigation-link_list_wrapper').removeClass('open');
+            $('.Navigation-overlay').removeClass('open');
+            $('.Navigation-overlay').addClass('closed');
         }
     });
 });
