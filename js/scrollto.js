68afe7a7afeb57d114d9311a219e7f1604f6565f
leahg725 <lgazes725@gmail.com>
2017-08-05 21:39:22 -0400

    changed top

--- js/scrollto.js
+++ js/scrollto.js
@@ -22,9 +22,7 @@ $(document).ready(function() {
           var $target = $(this.hash);
           $target = $target.length && $target || $('[name=' + this.hash.slice(1) +']');
           if ($target.length) {
-            var height = $('.Navigation').outerHeight(true);
-            var siteHeaderOffset = $('.Navigation').outerHeight(true) * 2;
-            var targetOffset = $target.offset().top;
+            var targetOffset = $target.offset().top - 20;
 
             $('html,body').animate({scrollTop: targetOffset}, 500);
                 return false;
